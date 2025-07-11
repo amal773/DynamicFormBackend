f7d10c1b46b05ae754d8a0f39952ca84e42883d9
amal773 <amalkuniyilparambath@gmail.com>
2025-07-11 14:55:39 +0530

    adding server.js-2

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ cds.on('bootstrap', app => {
   app.get('/', (req, res) => {
     res.send('CAP Backend is running 🚀');
   });
+
+  app.get('/catalog', (req, res) => {
+    res.redirect('/odata/v4/CatalogService');
+  });
 });
 
 module.exports = cds.server;
